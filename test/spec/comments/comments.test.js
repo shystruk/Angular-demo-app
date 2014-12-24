e4441708fb8e5b8182cda4c33648d531f7c8823b
Vasyl Stokolosa <v.stokolosa@sysiq.com>
2014-12-24 16:31:06 +0200

    Unit test for commentsController

--- test/spec/comments/comments.test.js
+++ test/spec/comments/comments.test.js
@@ -2,3 +2,45 @@
  * Created by v.stokolosa on 12/19/14.
  */
 'use strict';
+
+describe('commentsCtrl', function () {
+    var scope,
+        commentsStore = '';
+
+    beforeEach(angular.mock.module('app'));
+    beforeEach(angular.mock.inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        $controller('commentsCtrl', {$scope: scope});
+
+        //add data to array
+        scope.yourName = 'Vasyl';
+        scope.yourThoughts = 'Awesome app.';
+        scope.commentsData.unshift({
+            name: scope.yourName,
+            thoughts: scope.yourThoughts
+        });
+    }));
+
+    it('commentsData should have JSON format', function () {
+        commentsStore = JSON.stringify(scope.commentsData);
+
+        expect(commentsStore).toBe('[{"name":"Vasyl","thoughts":"Awesome app."}]');
+    });
+
+    it('count shouldn"t be ziro', function () {
+        var count = 0;
+
+        scope.counts = function () {
+            return count = scope.commentsData.length;
+        };
+        scope.counts();
+
+        expect(count).not.toEqual(0);
+    });
+
+    it('should delete comment whick click', function () {
+        scope.deleteComment(scope.yourName);
+
+        expect(scope.commentsData.length).toEqual(0);
+    });
+});
